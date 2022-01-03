28b031b566c93d1dc5727179bdd272cfdae686fb
Aaron <fong_lun@live.com>
2022-01-03 18:00:12 -0500

    auth added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute'
+
 
 function App() {
   return (
@@ -14,13 +18,27 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />}></Route>
           <Route path='/offers' element={<Offers />}></Route>
-          <Route path='/profile' element={<SignIn />}></Route>
-          <Route path='/sign-in' element={<Profile />}></Route>
+          <Route path='/sign-in' element={<SignIn />}></Route>
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
           <Route path='/sign-up' element={<SignUp />}></Route>
           <Route path='/forgot-password' element={<ForgotPassword />}></Route>
         </Routes>
         <Navbar />
       </Router>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </>
   );
 }
